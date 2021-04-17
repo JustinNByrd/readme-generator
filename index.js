9e462dadb81cf76c34e92b9780e0023a52123b6d
Justin Byrd <justin_byrd@hotmail.com>
2021-04-17 16:50:53 -0400

    Installed dependancy and added title question

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-// TODO: Include packages needed for this application
+// include the inquirer package and generateMarkdown
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then( (response) => {
+            console.log(`your title is: ${response.title}`);
+            }
+        );
+}
 
 // Function call to initialize app
 init();
