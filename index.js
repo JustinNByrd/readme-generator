47e652035183fb672d51a4c9793f655b7ed47caf
Justin Byrd <justin_byrd@hotmail.com>
2021-04-17 17:18:50 -0400

    Added functionality to writeToFile() and call to it

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-// include the inquirer package and generateMarkdown
+// include the inquirer and filesystem packages and generateMarkdown
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+// include generateMarkdown code
 const generateMarkdown = require('./utils/generateMarkdown');
+
+// file to be generated
+const outputFile = './output/README.md';
 
 // array of questions to pass to inquirer
 const questions = [
@@ -16,8 +22,12 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// write generated markdown to file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success! Your README.md file is available in the output directory.')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -26,6 +36,7 @@ function init() {
         .then( (response) => {
             const generatedMarkdown = generateMarkdown(response);
             console.log(generatedMarkdown);
+            writeToFile(outputFile, generatedMarkdown);
             }
         );
 }
